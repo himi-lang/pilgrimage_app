02d6c3edf9d63410e5e617df3fc978847ac1ba98
Cassia39 <kamikami1417@gmail.com>
2025-05-23 19:09:26 +0900

    修正バージョン

--- pilgrimage_app/lib/main.dart
+++ pilgrimage_app/lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'map_screen.dart';  // さっき作ったファイルを読み込む
+import 'map_screen.dart'; // さっき作ったファイルを読み込む
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  //
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '聖地巡礼アプリ',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MapScreen(),  // ← map_screen.dart の画面を最初に表示
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MapScreen(), // ← map_screen.dart の画面を最初に表示
     );
   }
 }
