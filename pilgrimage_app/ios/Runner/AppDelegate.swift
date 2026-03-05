54563dfaabaadb8e75f9a1ee323443966b7a8abf
Cassia39 <134718491+Cassia1225@users.noreply.github.com>
2026-03-05 15:38:18 +0900

    macOSでiphoneに出力できるように修正
    一度iosディレクトリを一掃し、再構築。
    info.plistにmapsのAPIを挿入。
    googleService-info.plistも追加。

--- pilgrimage_app/ios/Runner/AppDelegate.swift
+++ pilgrimage_app/ios/Runner/AppDelegate.swift
@@ -1,13 +1,19 @@
+import Flutter
 import GoogleMaps
-@UIApplicationMain
-class AppDelegate: FlutterAppDelegate {
+import UIKit
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if let key = Bundle.main.object(forInfoDictionaryKey: "GMSApiKey") as? String {
-      GMSServices.provideAPIKey(key)
-    }
+
+    GMSServices.provideAPIKey("AIzaSyC0HBKOGGy2AdQyguNGwFYVzcphkmbfF8c")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
